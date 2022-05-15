0871ad79e6f164386644ba486940a69e31b6600d
Dabsia <dabojohnson@gmail.com>
2022-05-14 19:07:02 -0700

    Applied Css Modules to App

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 // our-domain.com/
 import Link from "next/link"
 import Head from 'next/head'
+import styles from  '../styles/Home.module.css'
 const HomePage = () => {
     return (
         <div>
             <Head>
                 <title>My First Next Js App</title>
             </Head>
-            <h1>The Home Page </h1>
+            <h1 className= {styles.homePageTitle}>The Home Page </h1>
             <Link href = '/profile'>Go To Profile Page</Link>
 
         </div>
